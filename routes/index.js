d41403b1a19ab2dc7215467b36941c845539958b
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-05 07:14:11 +0700

    fix: undefined endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const NotFoundError = require('../errors/not-found');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Не найдено'));
+});
 
 module.exports = router;
