c82b7f86ea004fd16017bfe6a1758766f5fbc403
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-05 13:27:14 +0700

    fix: removed if (err) condition in errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,20 +1,16 @@
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  if (err) {
-    // отправка сообщения об ошибке
-    const { statusCode = 500, message } = err;
+  // отправка сообщения об ошибке
+  const { statusCode = 500, message } = err;
 
-    res
-      .status(statusCode)
-      .send({
+  res
+    .status(statusCode)
+    .send({
       // проверяем статус и выставляем сообщение в зависимости от него
-        message: statusCode === 500
-          ? 'На сервере произошла ошибка'
-          : message,
-      });
-  } else {
-    // передача управления следующему middleware или route handler
-    next();
-  }
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 };
 
 module.exports = errorHandler;
