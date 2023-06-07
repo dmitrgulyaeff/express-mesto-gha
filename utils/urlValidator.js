fdf5f2d48a57f52a6eaa5f8775ec8ce76827cfb4
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-08 03:45:19 +0700

    feat: added urlValidator

---
+++ utils/urlValidator.js
@@ -0,0 +1,7 @@
+module.exports.urlValidator = (string) => {
+  const regex = /https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i;
+  if (regex.test(string)) {
+    return string;
+  }
+  throw new Error('Введен некорректный url');
+};
