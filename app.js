3c127ca3b91b40b164176c153fd884831d3cbb83
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-08 06:00:59 +0700

    refactor: moved routers to routes/index.js

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
 const express = require('express');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { login, createUser } = require('./controllers/users');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
-const auth = require('./middlewares/auth');
-const { urlValidator } = require('./utils/urlValidator');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().custom(urlValidator),
-    }),
-  }),
-  createUser,
-);
-app.use(auth);
-
 app.use(routes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-app.use(errors());
-app.use(errorHandler);
