92dd42b3522e303942a02bb0533d72b3319d0973
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-07 19:08:43 +0700

    feat: added login and signup endpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { login, createUser } = require('./controllers/users');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -8,6 +9,10 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
 app.use((req, res, next) => {
   req.user = {
     _id: '647cc1c529bc4fef151c6d65',
