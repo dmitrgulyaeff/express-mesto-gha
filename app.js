3929625c645acf684f2b14ec3f959c9fe5569f0b
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-07 20:27:14 +0700

    feat: added auth middleware

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { login, createUser } = require('./controllers/users');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.post('/signin', login);
 app.post('/signup', createUser);
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '647cc1c529bc4fef151c6d65',
-  };
-
-  next();
-});
+app.use(auth);
 
 app.use(routes);
 
---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jsonwebtoken = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorized');
+
+module.exports = (req, res, next) => {
+  const { jwt } = req.cookies;
+
+  if (!jwt) {
+    console.log('jwt', jwt);
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
+  }
+
+  let payload;
+
+  try {
+    payload = jsonwebtoken.verify(jwt, 'super-strong-secret');
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
+  }
+
+  req.user = payload;
+  next();
+};
