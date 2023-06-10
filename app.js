da8c8637d37d640279f460fd974664546727c866
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-10 19:29:59 +0700

    feat: Added logger, cors and dotenv

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(requestLogger);
 
 app.use(routes);
 
 mongoose.connect(DB_URL);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
